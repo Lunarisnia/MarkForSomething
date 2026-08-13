05b106540237164523a1eb6117dbc6e47bd7a07d
Lunarisnia <50898372+Lunarisnia@users.noreply.github.com>
2026-08-13 21:24:22 +0700

    minimal implementation for interacting with the inventory api

---
+++ MarkForSomething/InventoryManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Dalamud.Game.Inventory;
+using Dalamud.Plugin.Services;
+
+namespace MarkForSomething;
+
+public class InventoryManager
+{
+    private static readonly GameInventoryType[] PlayerInventory =
+    [
+        GameInventoryType.Inventory1,
+        GameInventoryType.Inventory2,
+        GameInventoryType.Inventory3,
+        GameInventoryType.Inventory4,
+    ];
+
+    public IReadOnlyList<InventoryItem> GetInventory()
+    {
+        var items = new List<InventoryItem>();
+
+        foreach (var inventoryType in PlayerInventory)
+        {
+            foreach (var item in Plugin.GameInventory.GetInventoryItems(inventoryType))
+            {
+                if (item.IsEmpty)
+                    continue;
+
+                items.Add(new InventoryItem(
+                    item.ContainerType,
+                    item.InventorySlot,
+                    item.ItemId,
+                    item.Quantity));
+            }
+        }
+
+        return items;
+    }
+
+    public readonly record struct InventoryItem(
+        GameInventoryType Container,
+        uint Slot,
+        uint ItemId,
+        int Quantity);
+}
--- MarkForSomething/Plugin.cs
+++ MarkForSomething/Plugin.cs
@@ -1,86 +1,64 @@
 ﻿using Dalamud.Game.Command;
+using Dalamud.Game.ClientState.Keys;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using System.IO;
-using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using MarkForSomething.Windows;
 
 namespace MarkForSomething;
 
 public sealed class Plugin : IDalamudPlugin
 {
     [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
-    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
-    [PluginService] internal static IPlayerState PlayerState { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] internal static IFramework Framework { get; private set; } = null!;
+    [PluginService] internal static IKeyState KeyState { get; private set; } = null!;
+    [PluginService] internal static IGameInventory GameInventory { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
-
-    public Configuration Configuration { get; init; }
-
-    public readonly WindowSystem WindowSystem = new("MarkForSomething");
-    private ConfigWindow ConfigWindow { get; init; }
-    private MainWindow MainWindow { get; init; }
+    private const string CommandName = "/pinventory";
+    private const VirtualKey InventoryKey = VirtualKey.F9;
+    private readonly InventoryManager inventoryManager;
+    private bool inventoryKeyWasDown;
 
     public Plugin()
     {
-        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-
-        // You might normally want to embed resources and load them from the manifest stream
-        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
-
-        ConfigWindow = new ConfigWindow(this);
-        MainWindow = new MainWindow(this, goatImagePath);
-
-        WindowSystem.AddWindow(ConfigWindow);
-        WindowSystem.AddWindow(MainWindow);
+        inventoryManager = new InventoryManager();
 
         CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
-            HelpMessage = "A useful message to display in /xlhelp"
+            HelpMessage = "Lists the items in your inventory."
         });
 
-        // Tell the UI system that we want our windows to be drawn through the window system
-        PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
-
-        // This adds a button to the plugin installer entry of this plugin which allows
-        // toggling the display status of the configuration ui
-        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
-
-        // Adds another button doing the same but for the main ui of the plugin
-        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUi;
-
-        // Add a simple message to the log with level set to information
-        // Use /xllog to open the log window in-game
-        // Example Output: 00:57:54.959 | INF | [MarkForSomething] ===A cool log message from MarkForSomething===
-        Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+        Framework.Update += OnFrameworkUpdate;
     }
 
     public void Dispose()
     {
-        // Unregister all actions to not leak anything during disposal of plugin
-        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
-        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
-        PluginInterface.UiBuilder.OpenMainUi -= ToggleMainUi;
-        
-        WindowSystem.RemoveAllWindows();
-
-        ConfigWindow.Dispose();
-        MainWindow.Dispose();
-
+        Framework.Update -= OnFrameworkUpdate;
         CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnFrameworkUpdate(IFramework framework)
+    {
+        var keyIsDown = KeyState[InventoryKey];
+        if (keyIsDown && !inventoryKeyWasDown)
+            ListInventory();
+
+        inventoryKeyWasDown = keyIsDown;
     }
 
     private void OnCommand(string command, string args)
     {
-        // In response to the slash command, toggle the display status of our main ui
-        MainWindow.Toggle();
+        ListInventory();
     }
-    
-    public void ToggleConfigUi() => ConfigWindow.Toggle();
-    public void ToggleMainUi() => MainWindow.Toggle();
+
+    private void ListInventory()
+    {
+        var items = inventoryManager.GetInventory();
+        Log.Information("Inventory contains {ItemCount} occupied slots:", items.Count);
+
+        foreach (var item in items)
+            Log.Information("{Container} slot {Slot}: item {ItemId} x{Quantity}",
+                item.Container, item.Slot, item.ItemId, item.Quantity);
+    }
 }
